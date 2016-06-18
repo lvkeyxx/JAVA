cd6d44644a31291ae919108efe86ce5197973609
lvkeyxx <1210426451@qq.com>
2016-06-18 23:06:14 +0800

    test while

---
+++ While.java
@@ -0,0 +1,17 @@
+public class While
+{
+  public static void main(String args[])
+  {
+    int num=0,i=1;
+    while(i<=100)
+    {
+      if(i%3==0)
+      {
+        System.out.println(i + " ");
+        num++;
+      }
+      i++;
+      if(num==5) break;
+    }
+  }
+}
